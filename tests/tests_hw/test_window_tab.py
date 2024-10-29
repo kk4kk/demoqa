94219c0af6b96204c0743c9eb5c49c6618d4c701
kk4kk <y.kalkamanov@gmail.com>
2024-10-30 00:29:54 +0300

    demoqa_hw_12

--- tests/tests_hw/test_window_tab.py
+++ tests/tests_hw/test_window_tab.py
@@ -8,6 +8,7 @@ def test_window_tab(browser):
     links.visit()
     assert links.link_home.get_text() == 'Home'
     assert links.link_home.get_dom_attribute('href') == 'https://demoqa.com'
+    assert links.link_home.get_dom_attribute('target') == '_blank'
     links.link_home.click()
     time.sleep(2)
     assert len(browser.window_handles) == 2
